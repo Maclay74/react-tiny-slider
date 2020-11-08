3d594049b9c18e22c3f295d23d75e23a229b42fb
Maclay <mishakozlov74@gmail.com>
2020-11-08 18:31:42 +0100

    move to dangerous html

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,47 @@
-// @ts-nocheck
 import React, { useRef, useState } from 'react'
-
 import Carousel from 'react-tiny-slider'
 
-const blocks = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
-const blocks2 = ['test7', 'test8', 'test9', 'test10', 'test11', 'test12']
-
+const blocks = ['test1', 'test2', 'test3', 'test4']
+const blocks2 = ['1', '2', '3', '4', '5', '6', '7']
 
 const App = () => {
 
   const carousel = useRef(null)
-  const [currentBlocks,setCurrentBlocks] = useState(blocks)
+  const [currentBlocks, setCurrentBlocks] = useState(blocks2)
 
-  const next = () => {
-    carousel.current?.goTo('next')
+  const response = {
+    '800': {
+      'items': 3
+    },
+    '1200': {
+      'items': 4
+    }
   }
 
   return (
     <div>
-      <div onClick={() => next()}>NEXT</div>
-      <div onClick={() => next()}>PREV</div>
-      <div onClick={() => setCurrentBlocks(blocks)}>UPDATE 1</div>
-      <div onClick={() => setCurrentBlocks(blocks2)}>UPDATE 2</div>
+      <div onClick={() => setCurrentBlocks(blocks)}>Blocks 1</div>
+      <div onClick={() => setCurrentBlocks(blocks2)}>Blocks 2</div>
 
-      <Carousel swipeAngle={false} loop={true} mode={'carousel'} items={3} mouseDrag ref={carousel} controls={false}>
-        {currentBlocks.map(block =>{
-          return <div className={'tns-item'} style={{ height: 100, backgroundColor: 'lightgray' }} key={block}> <div className={'slide'}>{block}</div> </div>
+      <Carousel
+        responsive={response}
+        swipeAngle={false}
+        items={3}
+        mouseDrag
+        ref={carousel}
+        controls={false}
+        nav={false}
+        onClick={(slideIndex, info, event) => {
+          console.log(slideIndex, info, event)
+        }}
+      >
+        {currentBlocks.map((block) => {
+          return <div
+            className={'tns-item'}
+            style={{ height: 100, backgroundColor: 'lightgray' }}
+          >
+            <div className={'slide'}>{block}</div>
+          </div>
         })}
       </Carousel>
 
--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,11 +1,20 @@
-import React, {CSSProperties, MouseEvent, useEffect, useRef} from 'react'
-import {tns} from 'tiny-slider/src/tiny-slider'
+import React, { CSSProperties, MouseEvent, useEffect, useRef } from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { tns } from 'tiny-slider/src/tiny-slider'
 import 'tiny-slider/dist/tiny-slider.css'
-import {TinySliderInfo, TinySliderInstance, TinySliderSettings} from 'tiny-slider'
-import {getCloneCountForLoop} from "./helpers";
+import {
+  TinySliderInfo,
+  TinySliderInstance,
+  TinySliderSettings
+} from 'tiny-slider'
 
 interface ReactTinySliderConfig extends TinySliderSettings {
-  onClick?: (slideClicked: Element | null, info: any, event: MouseEvent) => void
+  onClick?: (
+    slideClicked: number | null,
+    info: any,
+    event: React.MouseEvent
+  ) => void
+  onInit?: () => void | false
   onIndexChanged?: (info: TinySliderInfo) => void
   onTransitionStart?: (info: TinySliderInfo) => void
   onTransitionEnd?: (info: TinySliderInfo) => void
@@ -14,56 +23,85 @@ interface ReactTinySliderConfig extends TinySliderSettings {
   onTouchEnd?: (info: TinySliderInfo) => void
   children: JSX.Element[] | JSX.Element
   className?: string
-  style?: CSSProperties,
+  style?: CSSProperties
 }
 
 const Carousel = React.forwardRef(
-  // @ts-ignore
   (props: ReactTinySliderConfig, ref: (ref: TinySliderInstance) => void) => {
     const slider = useRef<TinySliderInstance | null>(null)
     const containerRef = useRef<HTMLDivElement>(null)
     const dragging = useRef<boolean>(false)
 
     const onClick = (event: MouseEvent) => {
-      const {onClick} = props
-
+      const { onClick } = props
       if (dragging.current || !onClick) return
+
+      // when only one element the slider doesnt init
       if (!slider.current) return onClick(null, null, event)
 
       const info = slider.current.getInfo()
-      const slideClicked = info.slideItems[info.index]
+
+      // @ts-ignore
+      const slideClickOffset = [...info.slideItems]
+        .filter((slide) => slide.classList.contains('tns-slide-active'))
+        // @ts-ignore
+        .findIndex((slide) => slide.contains(event.target))
+      const slideClicked = (info.index + slideClickOffset) % info.slideCount
+
+      // call click callback with info and slide clicked
       onClick(slideClicked, info, event)
     }
 
     const build = (newSettings: ReactTinySliderConfig) => {
-
-
       const settings: ReactTinySliderConfig = {
         ...newSettings,
         onInit: () => {
-          // settings.onInit && settings.onInit()
-          postInit()
+          setTimeout(postInit, 0)
         },
-        container: containerRef.current as HTMLElement,
+        container: containerRef.current?.firstChild?.firstChild as HTMLElement
       }
-
       slider.current = tns(settings)
+    }
+
+    const generateMarkup = () => {
+      return ReactDOMServer.renderToStaticMarkup(
+        <div>
+          <div>{props.children}</div>
+        </div>
+      )
     }
 
     const postInit = () => {
       const events = slider.current?.events
 
+      const {
+        onIndexChanged,
+        onTransitionStart,
+        onTransitionEnd,
+        onTouchStart,
+        onTouchMove,
+        onTouchEnd,
+        onInit
+      } = props
+
       if (events) {
-        events.on('transitionStart', (info: TinySliderInfo) => {
+        events.on('dragMove', (info: TinySliderInfo) => {
           dragging.current = true
-          props.onTransitionStart && props.onTransitionStart(info)
+          onTransitionStart && onTransitionStart(info)
         })
 
-        events.on('transitionEnd', (info: TinySliderInfo) => {
+        events.on('dragEnd', (info: TinySliderInfo) => {
           dragging.current = false
-          props.onTransitionEnd && props.onTransitionEnd(info)
+          onTransitionEnd && onTransitionEnd(info)
         })
+
+        if (onIndexChanged) events.on('indexChanged', onIndexChanged)
+        if (onTouchStart) events.on('touchStart', onTouchStart)
+        if (onTouchMove) events.on('touchMove', onTouchMove)
+        if (onTouchEnd) events.on('touchEnd', onTouchEnd)
       }
+
+      onInit && onInit()
 
       setTimeout(() => {
         // @ts-ignore
@@ -73,60 +111,16 @@ const Carousel = React.forwardRef(
 
     useEffect(() => {
       build(props)
-
-      return () => {
-        slider.current && slider.current.destroy()
-      }
     }, [props])
-
-    const generateSlides = (children: JSX.Element[] | JSX.Element) => {
-      const slideCount = React.Children.count(children);
-      const cloneCount = props.loop ? getCloneCountForLoop(props, slideCount) : 0
-      const beforeClones = [];
-      const afterClones = [];
-
-      for (let j = cloneCount; j--;) {
-        const num = j % slideCount;
-
-        if (children !== undefined) {
-          const cloneFirst = React.cloneElement(children[num], {
-            className: "tns-item tns-slide-cloned",
-            id: null,
-            key: 'item-clone-first' + children[num].key
-          });
-
-          afterClones.unshift(cloneFirst)
-
-          if (props.mode === 'carousel') {
-            const cloneLast = React.cloneElement(children[slideCount - 1 - num], {
-              className: "tns-item tns-slide-cloned",
-              id: null,
-              key: 'item-clone-last' + children[num].key
-            });
-            beforeClones.push(cloneLast);
-          }
-        }
-      }
-
-      return [
-        ...beforeClones,
-        ...React.Children.toArray(children),
-        ...afterClones
-      ];
-    }
 
     return (
       <div
         onClick={onClick}
-        className={'tns-outer'}
-        id={'tns1-ow'}
-      >
-        <div className={'tns-ovh'} id={'tns1-mw'}>
-          <div className={'tns-inner'} id={'tns1-iw'} ref={containerRef}>
-            {generateSlides(props.children)}
-          </div>
-        </div>
-      </div>
+        dangerouslySetInnerHTML={{
+          __html: generateMarkup()
+        }}
+        ref={containerRef}
+      />
     )
   }
 )
